edf14b56b37cff7c06b1e7743d1efc930ac6a233
Diego <cornejodl13@gmail.com>
2023-11-21 22:22:31 -0600

    able to load in notes html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express'); //importing express as a variable. 
+const express = require('express'); 
 const fs = require('fs');
 const db = require('./db/db.json');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +10,46 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); 
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            res.json(err)
+        }
+        else {
+            data = JSON.parse(data);
+            res.json(data);
+        }
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        //adding unique id for new notes
+        id: Math.round(Math.random() * 10000),
+      };
+      //pushing array to db.json
+      db.push(newNote);
+      //Saving to db.json of the writing to be permanent.
+      fs.writeFile("./db/db.json", db, (err, data) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          //Route Status 200, good! And parse the data in JSON format.
+          res.status(200).json(data);
+        }
+      });
+});
 
 
 app.listen(PORT, () =>
